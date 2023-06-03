cd6f5c2f794334ca87bb62e61329f61ead5309ad
Web3 Name Service (WNS) <98871318+web3nameservice@users.noreply.github.com>
2023-06-03 23:28:53 +0530

    dc

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useContext } from "react";
 import Home from "./2.views/1.home/home";
 import Header from "./2.views/0.global/header/header";
 import Footer from "./2.views/0.global/footer/footer";
-import BgImg from "./1.resources/3.files/images/bg3.gif";
+import BgImg from "./1.resources/3.files/images/bg_alt.png";
+import BgVid from "./1.resources/3.files/images/bg1.mp4";
 import Presale from "./2.views/2.presale/presale";
 import Links from "./2.views/5.links/links";
 
@@ -11,18 +12,21 @@ class Main extends React.Component {
 
   render() {
     return (
-      <div style={{}} className="min-h-screen bg-black" style={{ backgroundImage: `url(${BgImg})`, backgroundPosition: "center center", backgroundSize: "cover", backgroundAttachment: "fixed" }}>
-        {window.location.pathname == "/links" ? <Links /> : (
-          <div className="flex flex-col justify-between min-h-screen">
+      <div style={{}} className="min-h-screen bg-black" style={{ backgroundImage: `url()`, backgroundPosition: "center center", backgroundSize: "cover", backgroundAttachment: "fixed" }}>
+        <div className="flex justify-center items-start">
+          <video autoPlay muted loop id="myVideo" className="fixed z-0 h-screen">
+            <source src={BgVid} type="video/mp4" className="" />
+            <img src={BgImg} alt="Alternate Image" />
+          </video>
+        </div>
+        <div className="flex flex-col justify-between min-h-screen">
 
-            <div>
-              {window.location.pathname == "/" ? <Home /> : null}
-              {window.location.pathname == "/presale" ? <Presale /> : null}
+          <div>
+            {window.location.pathname == "/" ? <Home /> : null}
+            {window.location.pathname == "/presale" ? <Presale /> : null}
 
-            </div>
           </div>
-        )}
-
+        </div>
 
       </div >
     );
