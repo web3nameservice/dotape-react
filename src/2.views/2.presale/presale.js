95143c8fe799f44ad43a0365e413734c89e6212a
Web3 Name Service (WNS) <98871318+web3nameservice@users.noreply.github.com>
2023-06-15 21:36:34 +0530

    Update presale.js

--- src/2.views/2.presale/presale.js
+++ src/2.views/2.presale/presale.js
@@ -28,18 +28,6 @@ const Presale = ({ walletConnected, setWalletConnected }) => {
         }
     }
 
-    async function checkInvestment() {
-        let amount = await callW3Api("/presale/credits/get", { address: address });
-        console.log(amount);
-        setInvestedAmount(parseFloat(amount.toString()));
-    }
-
-    useEffect(() => {
-        if (address != null && address != "" && address != undefined) {
-            init();
-            checkInvestment();
-        }
-    }, [address])
 
     useEffect(() => {
         document.title = "Presale - DOT APE";
@@ -59,68 +47,10 @@ const Presale = ({ walletConnected, setWalletConnected }) => {
                         <p className="pt-4 text-lg text-main font-bold">The .APE presale is open for everyone to participate in.</p>
 
                         <div className="mt-4 bg-white/10 px-10 py-10 rounded-xl border-2 border-white/10">
-
-                            {address == null ? (
-                                <div>
-                                    <p className="text-2xl font-bold">Presale</p>
-                                    <div className="pt-8">
-                                        <p className="text-sm">Before you participate, please keep in mind:</p>
-                                        <div className="pt-4">
-                                            <Points text="You can participate using ETH or Apecoin." />
-                                            <Points text="You can buy credits for any amount above 0.01 ETH or equivalent in Apecoin." />
-                                            <Points text="After the presale ends, you will be able to register any number of domains equivalent to the amount of tokens you put in presale." />
-                                            <Points text="You can buy credits as many times as you want." />
-                                        </div>
-                                    </div>
-                                    <p className="text-sm mt-4 text-amber-400">Connect your wallet to participate in the presale.</p>
-                                    <div className="mt-8">
-                                        <ConnectWallet />
-                                    </div>
-                                    <p className="text-sm pt-8 text-gray-400 break-words">Alternatively, you can also send ETH or Apecoin directly to dotapelabs.eth (0xaC8A1B5F9a3dc19C4Ef46ECA61E5FA58507b42d1)</p>
-
-                                </div>
-                            ) : (
-                                <div>
-                                    <div className="block md:flex justify-between items-center border-b-2 border-white/10 pb-6">
-                                        <div>
-                                            <p className="text-2xl font-semibold">Welcome {address != null ? domain == "" ? shortenaddress(address) : domain : ""}!</p>
-                                            <p className="text-gray-400 text-sm pt-1">Use this portal to participate in .APE presale</p>
-                                        </div>
-                                        <div className="mt-4 md:mt-0">
-                                            <ConnectWallet />
-                                        </div>
-                                    </div>
-
-                                    <div className="pt-8">
-                                        <p className="text-md text-main font-bold">Your current credits: {investedAmount > 0 ? calculateZeroes(investedAmount / 1e18) : "0.00"} ETH</p>
-                                    </div>
-                                    <div className="pt-8">
-                                        <p className="text-sm">Before you participate, please keep in mind:</p>
-                                        <div className="pt-4">
-                                            <Points text="You can participate using ETH or Apecoin." />
-                                            <Points text="You can buy credits for any amount above 0.01 ETH or equivalent in Apecoin." />
-                                            <Points text="After the presale ends, you will be able to register any number of domains equivalent to the amount of tokens you put in presale." />
-                                            <Points text="You can buy credits as many times as you want." />
-                                        </div>
-                                    </div>
-
-                                    <div className="pt-8">
-                                        <div className="bg-main px-4 py-2 rounded-full w-fit flex items-center gap-x-2" onClick={() => setInvestModalOpen(true)}>
-                                            <p className="text-white text-sm">{investedAmount > 0 ? "Buy credits" : "Buy credits"}</p>
-                                            <FontAwesomeIcon icon={['fas', 'arrow-right']} className="text-xs text-white" />
-                                        </div>
-                                    </div>
-
-                                </div>
-                            )}
-                            {/* {Date.now() < endTime * 1000 ? (
-                        <p className="text-main font-bold pt-8">Presale ends in : <span>{timeToString(endTime * 1000)}</span></p>
-                    ) : (
-                        <p className="text-main font-bold pt-8">Presale concluded</p>
-                    )} */}
-                            {/* <div>
-                                <p className="text-lg">The presale has not started yet.</p>
-                            </div> */}
+                    
+                             <div>
+                                <p className="text-lg">Presale has ended</p>
+                            </div> 
                         </div>
                         {/* <p className="pt-8 text-lg text-white font-bold">What is the .APE presale?</p> */}
                         <p className="text-lg pt-8">This presale gives participants a unique chance to register .APE domains before they become available to the public. Anyone can participate in the presale and put in ETH to secure their position. </p>
